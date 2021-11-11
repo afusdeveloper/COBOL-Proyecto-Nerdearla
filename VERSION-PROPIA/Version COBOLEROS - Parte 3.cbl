01198cf5fe18cfd915ccc291ad00dc1cd0faaa4a
Aymara Micaela Fusaro <70449334+afusdeveloper@users.noreply.github.com>
2021-11-10 22:39:56 -0300

    Update Version COBOLEROS - Parte 3.cbl

--- VERSION-PROPIA/Version COBOLEROS - Parte 3.cbl
+++ VERSION-PROPIA/Version COBOLEROS - Parte 3.cbl
@@ -1,16 +1,13 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID.                    'MI VERSION EJERCICIO3'.
        AUTHOR.                                AYMARA M FUSARO.
-
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-
          01 WSC-CONSTANTES.
              05 WSC-SENIORITY.
                 10 WSC-SENIOR                 PIC X(06) VALUE 'SENIOR'.
                 10 WSC-SEMISR                 PIC X(06) VALUE 'SEMISR'.
                 10 WSC-JUNIOR                 PIC X(06) VALUE 'JUNIOR'.
-
              05 WSC-SUELDOS.
                  10 WSC-SUELDO-SENIOR         PIC 9(06) VALUE 200000.
                  10 WSC-SUELDO-SEMISR         PIC 9(06) VALUE 160000.
@@ -57,7 +54,6 @@
                  10 WSC-ANUAL3                PIC X(07) VALUE ALL '-'. 
                  10 FILLER                    PIC X(10) VALUE SPACES.
                  10 WSC-BONO3                 PIC X(06) VALUE ALL '-'. 
-
          01 WSV-VARIABLES.
              05 WSV-POSTULANTES.
                  10 WSV-POSTU1.
@@ -80,7 +76,6 @@
                      15 WSV-SUELDO3           PIC 9(06).
                      15 WSV-SUELDO-ANUAL3     PIC 9(07).
                      15 WSV-BONO3             PIC 9(06).
-
              05 WSV-POSTULANTE-AUX.
                  10 WSV-NOMBRE-AUX            PIC X(05).
                  10 WSV-EXPERIENCIA-AUX       PIC 9(02).
@@ -89,16 +84,13 @@
                  10 WSV-SUELDO-AUX            PIC 9(06).
                  10 WSV-SUELDO-ANUAL-AUX      PIC 9(07).
                  10 WSV-BONO-AUX              PIC 9(06).
-
   
        PROCEDURE DIVISION.
-
        00-CONTROL.
             PERFORM 10-INICIO.
             PERFORM 20-PROCESO.
             STOP RUN.
-       00-END. EXIT.
-
+       00-CONTROL-END. EXIT.
        10-INICIO.
             DISPLAY WSC-GUIONES.
             DISPLAY WSC-TIT-1.
@@ -106,8 +98,7 @@
             DISPLAY WSC-COLUMNAS.
             DISPLAY WSC-GUIONES2.
             INITIALIZE WSV-POSTULANTE-AUX.
-       10-END. EXIT.
-
+       10-INICIO-END. EXIT.
        20-PROCESO.
             MOVE WSV-POSTU1 TO WSV-POSTULANTE-AUX.
             PERFORM 25-EVALUAR.
@@ -120,8 +111,7 @@
             MOVE WSV-POSTU3 TO WSV-POSTULANTE-AUX.
             PERFORM 25-EVALUAR.
             PERFORM 30-CALCULAR-SUELDO.
-       20-END. EXIT.
-
+       20-PROCESO-END. EXIT.
        25-EVALUAR.
             EVALUATE TRUE
             WHEN WSS-EXP-JUNIOR 
@@ -131,25 +121,23 @@
             WHEN OTHER 
                 MOVE WSC-SUELDO-SENIOR TO WSV-SUELDO-AUX
             END-EVALUATE.
-       25-END. EXIT.
-
+       25-EVALUAR-END. EXIT.
        30-CALCULAR-SUELDO.
            MULTIPLY 12 BY WSV-SUELDO-AUX 
                           GIVING WSV-SUELDO-ANUAL-AUX
-
             EVALUATE WSV-EXPERIENCIA-AUX
                 WHEN 0
                     MOVE 0 TO WSV-BONO-AUX
                     PERFORM 35-ESCRIBIR-JUNIOR THRU 35-END
                 WHEN 1
+                WHEN 2
                     COMPUTE WSV-BONO-AUX = WSV-SUELDO-AUX * 1.5
                     PERFORM 35-ESCRIBIR-JUNIOR THRU 35-END
-                WHEN 2
                 WHEN 3
-                    COMPUTE WSV-BONO-AUX = WSV-SUELDO-AUX * 2
-                    PERFORM 40-ESCRIBIR-SEMISR THRU 40-ESCRIBIR-SEMISR
                 WHEN 4
                 WHEN 5
+                    COMPUTE WSV-BONO-AUX = WSV-SUELDO-AUX * 2
+                    PERFORM 40-ESCRIBIR-SEMISR THRU 40-ESCRIBIR-SEMISR
                 WHEN 6
                     COMPUTE WSV-BONO-AUX = WSV-SUELDO-AUX * 2.5
                     PERFORM 45-ESCRIBIR-SENIOR THRU 45-END
@@ -157,9 +145,8 @@
                     COMPUTE WSV-BONO-AUX = WSV-SUELDO-AUX * 3 
                     PERFORM 45-ESCRIBIR-SENIOR THRU 45-END
             END-EVALUATE.
-
             INITIALIZE WSV-POSTULANTE-AUX.
-       30-END. EXIT.
+       30-CALCULAR-SUELDO-END. EXIT.
        
        35-ESCRIBIR-JUNIOR.
             DISPLAY ' '
